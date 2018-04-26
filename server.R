499d650f7fe552515d7329eada3db543e94a45f2
Koki Ando <basket.koki.bowl@gmail.com>
2018-04-27 00:02:13 +1000

    server side code added

--- server.R
+++ server.R
@@ -2,5 +2,21 @@ library(shiny)
 library(tidyverse)
 
 server <- function(input, output){
+  output$table <- renderTable({
+    players_list_since1950 %>% 
+      filter(Player == as.character(input$player_name)) %>% 
+      select(-given_name, -family_name, -key, -img_url)
+  })
   
+  image <- 
+    reactive({image_read(players_list_since1950 %>% 
+                           filter(Player == as.character(input$player_name)) %>% 
+                           select(img_url) %>% 
+                           as.character())})
+  
+  output$img <- renderImage({
+    tmpfile <- image() %>%
+      image_write(tempfile(fileext='jpg'), format = 'jpg')
+    list(src = tmpfile, contentType = "image/jpeg")
+  })
 }
