3739a6e2b8842b330ef98ff965725cce1de2ccad
Koki Ando <basket.koki.bowl@gmail.com>
2018-04-28 13:25:52 +1000

    career summary data input

--- global.R
+++ global.R
@@ -3,10 +3,12 @@ library(tidyverse)
 library(SportsAnalytics)
 library(stringr)
 library(magick)
+library(shinydashboard)
 
 players_list_since1950 <- fread("https://s3-ap-southeast-2.amazonaws.com/koki25ando/Players.csv", data.table = FALSE)
 players_list_since1950 <- players_list_since1950 %>% select(-V1)
 
+## Profile Picture 
 ## key setting
 players_list_since1950 <- players_list_since1950 %>% 
   separate(Player, sep = " ", 
@@ -27,5 +29,21 @@ players_list_since1950 <- players_list_since1950 %>%
            paste0("https://d2cwpp38twqe55.cloudfront.net/req/201804182/images/players/", key, "01.jpg"))
 
 
+####----------------
+players_season_stats <- fread("https://s3-ap-southeast-2.amazonaws.com/playerinfomation/Seasons_Stats.csv", data.table = FALSE)
+players_season_stats <- players_season_stats %>% select(-V1)
 
+players_season_stats <- players_season_stats %>% 
+  select(-contains("WS"), -contains("STL%"), -contains("RB%"),-blanl,
+         -contains("BPM"), -contains("eFG%"), -contains("AST%"), -contains("BLK%"), 
+         -contains("TOV%"), -contains("USG%"), -blank2, -contains("TS%"), 
+         -contains("3PAr"), -VORP, -GS)
 
+players_season_stats <- as.data.frame(players_season_stats)
+career_summary_stats <- 
+  players_season_stats %>% 
+  group_by(Player) %>% 
+  mutate(PPG = sum(PTS)/sum(G), RPG = sum(TRB)/sum(G), APG = sum(AST)/sum(G), SPG = sum(STL)/sum(G)) %>% 
+  select(Player, PPG:SPG) %>% 
+  distinct(Player, .keep_all = TRUE)
+career_summary_stats <- as.data.frame(career_summary_stats)
