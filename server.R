21ad1c2308bd04bc6672712078d9bef85c066364
Koki Ando <basket.koki.bowl@gmail.com>
2018-05-02 21:43:58 +1000

    ggploty code added

--- server.R
+++ server.R
@@ -21,19 +21,21 @@ server <- function(input, output){
     career_summary_stats %>% filter(Player == as.character(input$player_name))
   })
   
-  output$career_plot <- renderPlot({
-    stats_advanced %>% 
+  output$career_plot <- renderPlotly({
+    career_plotly <- stats_advanced %>% 
       filter(Player == as.character(input$player_name)) %>% 
       ggplot(aes(x = Year, y = PPG)) + 
       geom_point() + 
       geom_line()
+    ggplotly(career_plotly)
   })
   
-  output$cumulative_plot <- renderPlot({
-    career_cumulative_stats %>% 
+  output$cumulative_plot <- renderPlotly({
+    career_cumulative_plotly <- career_cumulative_stats %>% 
       filter(Player == as.character(input$player_name)) %>% 
       ggplot(aes(x = Year, y = Career_PTS)) + 
       geom_point() +
       geom_step()
+    ggplotly(career_cumulative_plotly)
   })
 }
